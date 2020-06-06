9d6e45a30ae13f9803e539605b500395ce6ec207
yasmin salah <50454254+YasminSalahEldin@users.noreply.github.com>
2020-06-06 05:49:25 +0200

    Add files via upload

---
+++ Splashscreen/app/src/main/java/com/example/android/splashscreen/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, homeActivity.class);
+                startActivity(homeIntent);
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
